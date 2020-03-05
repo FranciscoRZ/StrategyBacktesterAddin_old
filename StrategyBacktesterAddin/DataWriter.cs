eed4ad6141192a3529c653db601a76274cee0532
Francisco REY ZALUAR <francisco.rey-zaluar@dauphine.eu>
2020-03-05 19:55:47 +0100

    Start DataWriter

---
+++ StrategyBacktesterAddin/DataWriter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Excel = Microsoft.Office.Interop.Excel;
+using ExcelDna;
+using Model = ThreeFourteen.AlphaVantage.Model;
+
+    namespace StrategyBacktesterAddin
+{
+    public class DataWriter
+    {
+        private Model.TimeSeriesEntry[] _data;
+
+        public void Write()
+        {
+
+        }
+
+        public DataWriter(Model.TimeSeriesEntry[] data)
+        {
+            this._data = data;
+        }
+    }
+}
